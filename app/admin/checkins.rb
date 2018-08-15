4acc4ab31c2f8654772166eb2c36406138a64030
Raghu Betina <raghu@firstdraft.com>
2018-08-15 21:09:49 +0000

    Whitelist columns in Checkin for ActiveAdmin

--- app/admin/checkins.rb
+++ app/admin/checkins.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Checkin do
+
+ permit_params :poet_id, :bout_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
