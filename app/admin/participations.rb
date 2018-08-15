8a3ded1d5da1fad27a9f8dfe217fcd7b291a5b56
Raghu Betina <raghu@firstdraft.com>
2018-08-15 21:08:55 +0000

    Whitelist columns in Participation for ActiveAdmin

--- app/admin/participations.rb
+++ app/admin/participations.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Participation do
+
+ permit_params :team_id, :competition_id, :finals, :semifinals, :quarterfinals
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
