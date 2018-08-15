e8d731367b5e0dd3bb5ba08ce360b12cf674cebc
Raghu Betina <raghu@firstdraft.com>
2018-08-15 21:09:33 +0000

    Whitelist columns in Deduction for ActiveAdmin

--- app/admin/deductions.rb
+++ app/admin/deductions.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Deduction do
+
+ permit_params :poem_id, :penalty_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
