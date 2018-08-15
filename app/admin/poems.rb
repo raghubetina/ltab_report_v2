f6871261a37246d722f32ca40bbbd53ab47cf015
Raghu Betina <raghu@firstdraft.com>
2018-08-15 21:09:13 +0000

    Whitelist columns in Poem for ActiveAdmin

--- app/admin/poems.rb
+++ app/admin/poems.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Poem do
+
+ permit_params :round_id, :poet_id, :minutes, :seconds, :notes
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
