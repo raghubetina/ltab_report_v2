cfe941eb2286d1aefc2b73172518de1a4833aa1c
Raghu Betina <raghu@firstdraft.com>
2018-08-15 21:08:44 +0000

    Whitelist columns in Poet for ActiveAdmin

--- app/admin/poets.rb
+++ app/admin/poets.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Poet do
+
+ permit_params :team_id, :first_name, :last_name, :phone_number, :email
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
