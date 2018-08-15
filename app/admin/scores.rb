99f1c9a9be0f1a892deacdc2ead0886f842b78e9
Raghu Betina <raghu@firstdraft.com>
2018-08-15 21:09:25 +0000

    Whitelist columns in Score for ActiveAdmin

--- app/admin/scores.rb
+++ app/admin/scores.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Score do
+
+ permit_params :position, :points, :poem_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
